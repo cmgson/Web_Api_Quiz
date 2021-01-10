dfcd381c220b71ca8ad230f6ad44c94b0782205e
Chris Gutierrez <cmgson1@gmail.com>
2021-01-09 18:13:16 -0600

    added variables and selectors in js

---
+++ script.js
@@ -0,0 +1,6 @@
+var answerList = document.querySelector("answerList");
+var answer1 = document.querySelector("a1");
+var answer2 = document.querySelector("a2");
+var answer3 = document.querySelector("a3");
+var answer4 = document.querySelector("a4");
+var question = document.querySelector("question")
