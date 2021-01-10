98506cb8085285faf2beff3ea825ee8d9e37e9eb
Chris Gutierrez <cmgson1@gmail.com>
2021-01-09 19:02:53 -0600

    toying with objects

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+// variables for queryselectors
+
 var answerList = document.querySelector("answerList");
 var answer1 = document.querySelector("a1");
 var answer2 = document.querySelector("a2");
 var answer3 = document.querySelector("a3");
 var answer4 = document.querySelector("a4");
-var question = document.querySelector("question")
+var question = document.querySelector("question");
+
+
+var key = {
+    quest: question,
+    ans1 : answer1,
+    ans2 : answer2,
+    ans3 : answer3,
+    ans4 : answer4
+}
+
+var question1 = {
+    quest: "what color is sky?",
+    right: "blue",
+    wrong1: "red",
+    wrong2: "green",
+    wrong3: "pink"
+};
+
+var question2 = {
+    quest: "what goes up?",
+    wrong1 : "must go left",
+    wrong2 : "must go right",
+    wrong3 : "must go middle",
+    right: "must come down"
+};
+
