9366c2464d5b4001f17cb0919820a612b0b09369
Chris Gutierrez <cmgson1@gmail.com>
2021-01-11 21:48:52 -0600

    added new question card functionality

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ $answerBtn3 = $("#btn3");
 $answerBtn4 = $("#btn4");
 $timer = $("#timer");
 $highScore = $("#highScore");
+$startBtn = $("#start");
 
-($answerBtns).on("click", function() {
+$('ul').hide();
+$question.hide();
+$startBtn.on('click', function(){
+    $startBtn.hide();
+    $('ul').show();
+    $question.show();
+})
+
+$answerBtn1.on("click", function() {
     card1();
-    // ($answerBtns).on("click", function() {
-    //     card2();
-    //     ($answerBtns).on("click", function() {
-    //         card3();
-    //         ($answerBtns).on("click", function() {
-    //             card4();
-    //         });
-    //     });
-    // });
 });
 
 
@@ -27,70 +27,59 @@ $highScore = $("#highScore");
 
 
 function card1() {
-    ($question).html("1");
-    ($answerBtn1).html("a1 card 1");
-    ($answerBtn2).html("a2 card 1");
-    ($answerBtn3).html("a3 card 1");
-    ($answerBtn4).html("a4 card 1");
-    ($answerBtn1).attr('class','.correct');
-    $($answerBtns).each(function(){
-        if($(this).is(".correct")){
-            $(this).on("click", function(){
+    $answerBtns.off("click");
+    $question.html("1");
+    $answerBtn1.html("a1 card 1");
+    $answerBtn2.html("a2 card 1");
+    $answerBtn3.html("a3 card 1");
+    $answerBtn4.html("a4 card 1");
+    $answerBtn1.attr('class','.correct');
+    $answerBtn1.on("click", function(){
                 card2();
-            });
-        };
-    });
+        });
+    $answerBtn1.removeClass(".correct");
 };
     
     
 
 
 function card2() {
+    $answerBtns.off("click");
     $question.html("2");
-    ($answerBtn1).html("a1 card 2");
-    ($answerBtn2).html("a2 card 2");
-    ($answerBtn3).html("a3 card 2");
-    ($answerBtn4).html("a4 card 2"); 
-    ($answerBtn2).attr('class','.correct');
-    $($answerBtns).each(function(){
-        if($(this).is(".correct")){
-            $(this).on("click", function(){
-                card3();
-            });
-        };
-    });
-    ($answerBtn2).removeClass(".correct");
+    $answerBtn1.html("a1 card 2");
+    $answerBtn2.html("a2 card 2");
+    $answerBtn3.html("a3 card 2");
+    $answerBtn4.html("a4 card 2"); 
+    $answerBtn2.attr('class','.correct');
+    $answerBtn2.on("click",function(){
+        card3();
+    })
+    $answerBtn2.removeClass(".correct");
 };
 
 function card3() {
+    $answerBtns.off("click");
     $question.html("3");
-    ($answerBtn1).html("a1 card 3");
-    ($answerBtn2).html("a2 card 3");
-    ($answerBtn3).html("a3 card 3");
-    ($answerBtn4).html("a4 card 3"); 
-    ($answerBtn3).attr('class','.correct');
-    $($answerBtns).each(function(){
-        if($(this).is(".correct")){
-            $(this).on("click", function(){
-                card4();
-            });
-        };
-    });
+    $answerBtn1.html("a1 card 3");
+    $answerBtn2.html("a2 card 3");
+    $answerBtn3.html("a3 card 3");
+    $answerBtn4.html("a4 card 3"); 
+    $answerBtn3.attr('class','.correct');
+    $answerBtn3.on("click",function(){
+        card4();
+    })
     ($answerBtn3).removeClass(".correct");
 };
 function card4() {
+    $answerBtns.off("click");
     $question.html("4");
     ($answerBtn1).html("a1 card 4");
     ($answerBtn2).html("a2 card 4");
     ($answerBtn3).html("a3 card 4");
     ($answerBtn4).html("a4 card 4");
     ($answerBtn4).attr('class','.correct');
-    $($answerBtns).each(function(){
-        if($(this).is(".correct")){
-            $(this).on("click", function(){
-                card1();
-            });
-        };
+    $answerBtn4.on("click",function(){
+        card1();
     });
     ($answerBtn4).removeClass(".correct");
 
