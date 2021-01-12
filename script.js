c44ed9d84bc7c20e4bcadd7a3ff70a31041df7d5
Chris Gutierrez <cmgson1@gmail.com>
2021-01-12 16:22:50 -0600

    maybe a fix for correct and incorrect

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $timer = $("#timer");
 $highScore = $("#highScore");
 $startBtn = $("#start");
 var $correct;
-$incorrect = $('#wrong');
+var $incorrect; //= $('.wrong');
 $answerList.hide();
 $question.hide();
 
@@ -26,7 +26,7 @@ $startBtn.on('click', function(){
     
 
 function card0() {
-    // $answerBtns.off("click");
+    $answerBtns.off('click');
     $question.html("0");
     $answerBtn1.html("a1 card 0");
     $answerBtn2.html("a2 card 0");
@@ -34,9 +34,10 @@ function card0() {
     $answerBtn4.html("a4 card 0");
     $answerBtn1.attr('class','correct');
     $correct = $('.correct');
-    // $incorrect = $('.wrong');
+    $incorrect = $('.wrong');
     $correct.on('click', function(){
         card1();
+        $answerBtn1.removeClass("correct").addClass('wrong');
     });
     $incorrect.on('click', function(){
         //decrement timer
@@ -49,17 +50,19 @@ function card0() {
 //decrement();
 
 function card1() {
-    // $answerBtns.off("click");
-    $question.html("0");
-    $answerBtn1.html("a1 card 0");
-    $answerBtn2.html("a2 card 0");
-    $answerBtn3.html("a3 card 0");
-    $answerBtn4.html("a4 card 0");
+    $answerBtns.off("click");
+    $question.html("1");
+    $answerBtn1.html("a1 card 1");
+    $answerBtn2.html("a2 card 1");
+    $answerBtn3.html("a3 card 1");
+    $answerBtn4.html("a4 card 1");
     $answerBtn1.attr('class','correct');
     $correct = $('.correct');
-    // $incorrect = $('.wrong');
+    $incorrect = $('.wrong');
     $correct.on('click', function(){
         card2();
+        $answerBtn1.removeClass("correct").addClass('wrong');
+        
     });
     
     $incorrect.on('click', function(){
@@ -72,67 +75,68 @@ function card1() {
 };
 
 function card2() {
-    // $answerBtns.off("click");
-    $question.html("0");
-    $answerBtn1.html("a1 card 0");
-    $answerBtn2.html("a2 card 0");
-    $answerBtn3.html("a3 card 0");
-    $answerBtn4.html("a4 card 0");
+    $answerBtns.off("click");
+    $question.html("2");
+    $answerBtn1.html("a1 card 2");
+    $answerBtn2.html("a2 card 2");
+    $answerBtn3.html("a3 card 2");
+    $answerBtn4.html("a4 card 2");
     $answerBtn2.attr('class','correct');
     $correct = $('.correct');
-   
-    // $incorrect.on('click', function(){
-    //     //decrement timer
-    //     console.log("decrement timer");
-    // });
-    
-    $correct.on('click', function(){
-
+   $correct.on('click', function(){
         card3();
+        $answerBtn2.removeClass("correct").addClass('wrong');
     });
+    $incorrect.on('click', function(){
+        //decrement timer
+        console.log("decrement timer");
+    });
+    
+    
      //decrement();
 };
 
 function card3() {
     // $answerBtns.off("click");
-    $question.html("0");
-    $answerBtn1.html("a1 card 0");
-    $answerBtn2.html("a2 card 0");
-    $answerBtn3.html("a3 card 0");
-    $answerBtn4.html("a4 card 0");
+    $question.html("3");
+    $answerBtn1.html("a1 card 3");
+    $answerBtn2.html("a2 card 3");
+    $answerBtn3.html("a3 card 3");
+    $answerBtn4.html("a4 card 3");
     $answerBtn3.attr('class','correct');
     $correct = $('.correct');
-    
-    // $incorrect.on('click', function(){
-    //     //decrement timer
-    //     console.log("decrement timer");
-    // });
-    
     $correct.on('click', function(){
         card4();
+        $answerBtn3.removeClass("correct").addClass('wrong');
     });
+    $incorrect.on('click', function(){
+        //decrement timer
+        console.log("decrement timer");
+    });
+    
+    
     //decrement();
 };
 
 function card4() {
     // $answerBtns.off("click");
-    $question.html("0");
-    $answerBtn1.html("a1 card 0");
-    $answerBtn2.html("a2 card 0");
-    $answerBtn3.html("a3 card 0");
-    $answerBtn4.html("a4 card 0");
+    $question.html("4");
+    $answerBtn1.html("a1 card 4");
+    $answerBtn2.html("a2 card 4");
+    $answerBtn3.html("a3 card 4");
+    $answerBtn4.html("a4 card 4");
     $answerBtn4.attr('class','correct');
     $correct = $('.correct');
-   
-    // $incorrect.on('click', function(){
-    //     //decrement timer
-    //     console.log("decrement timer");
-    // });
-    
     $correct.on('click', function(){
-        console.log("test");
+        $answerBtn4.removeClass("correct").addClass('wrong');
         card0();
     });
+    $incorrect.on('click', function(){
+        //decrement timer
+        console.log("decrement timer");
+    });
+    
+    
      //decrement();
 };
 
