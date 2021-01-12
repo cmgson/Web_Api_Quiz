6a00fd3357462fd7f2277875d13f15ac2a600b75
Chris Gutierrez <cmgson1@gmail.com>
2021-01-11 20:25:37 -0600

    ditched objects testing functionality with button switching nested with questions

--- script.js
+++ script.js
@@ -1,34 +1,102 @@
-// variables for queryselectors
+// jquery cache
+$question = $("#question");
+$answerList = $("#answerList");
+$answerBtns = $(":button");
+$answerBtn1 = $("#btn1");
+$answerBtn2 = $("#btn2");
+$answerBtn3 = $("#btn3");
+$answerBtn4 = $("#btn4");
+$timer = $("#timer");
+$highScore = $("#highScore");
 
-var answerList = document.querySelector("answerList");
-var answer1 = document.querySelector("a1");
-var answer2 = document.querySelector("a2");
-var answer3 = document.querySelector("a3");
-var answer4 = document.querySelector("a4");
-var question = document.querySelector("question");
+($answerBtns).on("click", function() {
+    card1();
+    // ($answerBtns).on("click", function() {
+    //     card2();
+    //     ($answerBtns).on("click", function() {
+    //         card3();
+    //         ($answerBtns).on("click", function() {
+    //             card4();
+    //         });
+    //     });
+    // });
+});
 
 
-var key = {
-    quest: question,
-    ans1 : answer1,
-    ans2 : answer2,
-    ans3 : answer3,
-    ans4 : answer4
-}
 
-var question1 = {
-    quest: "what color is sky?",
-    right: "blue",
-    wrong1: "red",
-    wrong2: "green",
-    wrong3: "pink"
+
+
+function card1() {
+    ($question).html("1");
+    ($answerBtn1).html("a1 card 1");
+    ($answerBtn2).html("a2 card 1");
+    ($answerBtn3).html("a3 card 1");
+    ($answerBtn4).html("a4 card 1");
+    ($answerBtn1).attr('class','.correct');
+    $($answerBtns).each(function(){
+        if($(this).is(".correct")){
+            $(this).on("click", function(){
+                card2();
+            });
+        };
+    });
+};
+    
+    
+
+
+function card2() {
+    $question.html("2");
+    ($answerBtn1).html("a1 card 2");
+    ($answerBtn2).html("a2 card 2");
+    ($answerBtn3).html("a3 card 2");
+    ($answerBtn4).html("a4 card 2"); 
+    ($answerBtn2).attr('class','.correct');
+    $($answerBtns).each(function(){
+        if($(this).is(".correct")){
+            $(this).on("click", function(){
+                card3();
+            });
+        };
+    });
+    ($answerBtn2).removeClass(".correct");
 };
 
-var question2 = {
-    quest: "what goes up?",
-    wrong1 : "must go left",
-    wrong2 : "must go right",
-    wrong3 : "must go middle",
-    right: "must come down"
+function card3() {
+    $question.html("3");
+    ($answerBtn1).html("a1 card 3");
+    ($answerBtn2).html("a2 card 3");
+    ($answerBtn3).html("a3 card 3");
+    ($answerBtn4).html("a4 card 3"); 
+    ($answerBtn3).attr('class','.correct');
+    $($answerBtns).each(function(){
+        if($(this).is(".correct")){
+            $(this).on("click", function(){
+                card4();
+            });
+        };
+    });
+    ($answerBtn3).removeClass(".correct");
 };
+function card4() {
+    $question.html("4");
+    ($answerBtn1).html("a1 card 4");
+    ($answerBtn2).html("a2 card 4");
+    ($answerBtn3).html("a3 card 4");
+    ($answerBtn4).html("a4 card 4");
+    ($answerBtn4).attr('class','.correct');
+    $($answerBtns).each(function(){
+        if($(this).is(".correct")){
+            $(this).on("click", function(){
+                card1();
+            });
+        };
+    });
+    ($answerBtn4).removeClass(".correct");
+
+};
+
+
+
+    
 
