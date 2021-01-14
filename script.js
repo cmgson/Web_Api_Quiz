b7e15c868fb3feb2bbf4eea5a4759553f7fa77a2
Chris Gutierrez <cmgson1@gmail.com>
2021-01-14 17:08:40 -0600

    added local storage function and high score card

--- script.js
+++ script.js
@@ -12,10 +12,11 @@ $startBtn = $("#start");
 $gameOver = $("#gameOver");
 
 // these are for the highscore list
-$initials = $('#initials');
-$highScore = $('#highScoreCard');
-$submitBtn = $('#submit');
-$lastEntries = $('#lastEntries');
+$initials = $("#initials");
+$highScore = $("#highScoreCard");
+$submitBtn = $("#submit");
+$lastEntries = $("#lastEntries");
+$scoreDisplaySpan = $('#scoreDisplay');
 
 // these are for the timer
 var $correct;
@@ -27,17 +28,34 @@ $answerList.hide();
 $question.hide();
 $highScore.hide();
 
+renderLastRegistered();
+
+function renderLastRegistered() {
+  var initials = localStorage.getItem("initials");
+
+  $lastEntries.text(initials + ": " + count);
+}
+
+$submitBtn.on("click", function (event) {
+  event.preventDefault();
+
+  var initialEntry = document.querySelector("#initials").value;
+  localStorage.setItem("initials", initialEntry);
+  renderLastRegistered();
+});
+
 //logic for end of game
-function endGame () {
-    if (count <= 0) {
-        clearInterval(timer);
-        $("ul").hide();
-        $question.hide();
-        $gameOver.show();
-        $gameOver.html("<h1>GAME OVER!!!</h1>");
-        $highScore.show();
-    };
-};
+function endGame() {
+  if (count <= 0) {
+    clearInterval(timer);
+    $("ul").hide();
+    $question.hide();
+    $gameOver.show();
+    $gameOver.html("<h1>GAME OVER!!!</h1>");
+    $scoreDisplaySpan.text('score');
+    $highScore.show();
+  }
+}
 
 //start button initialized show game
 $startBtn.on("click", function () {
@@ -62,22 +80,21 @@ $startBtn.on("click", function () {
 function decrement() {
   count -= 10;
 }
+// abstracted away some timer stuff
 function timerStuff() {
-    console.log("decrement timer");
-    if (count >= 10) {
-      decrement();
-    } else {
-      clearInterval(timer);
-      count = 0;
-      $timer.html(count);
-      $("ul").hide();
-      $question.hide();
-      $gameOver.html("<h1>GAME OVER!!!</h1>");
-      $highScore.show();
-        };
-};
-
-
+  console.log("decrement timer");
+  if (count >= 10) {
+    decrement();
+  } else {
+    clearInterval(timer);
+    // count = 0;
+    $timer.html(count);
+    $("ul").hide();
+    $question.hide();
+    $gameOver.html("<h1>GAME OVER!!!</h1>");
+    $highScore.show();
+  }
+}
 
 function card0() {
   $question.html("0");
@@ -95,43 +112,40 @@ function card0() {
 
   $(".correct").on("click", function () {
     $(".correct,.wrong").off();
-    
-    
+
     $answerBtn1.removeClass("correct");
     $answerBtn2.removeClass("wrong");
     $answerBtn3.removeClass("wrong");
     $answerBtn4.removeClass("wrong");
     card1();
-    });
+  });
 
-    $(".wrong").on("click", function () {
-        timerStuff();
-    });
-
-  
+  $(".wrong").on("click", function () {
+    timerStuff();
+  });
 }
 
 function card1() {
-    $question.html("1");
-    $answerBtn1.html("a1 card 1");
-    $answerBtn1.addClass("correct");
-  
-    $answerBtn2.html("a2 card 1");
-    $answerBtn2.addClass("wrong");
-  
-    $answerBtn3.html("a3 card 1");
-    $answerBtn3.addClass("wrong");
-  
-    $answerBtn4.html("a4 card 1");
-    $answerBtn4.addClass("wrong");
-  
-    $("#correct").on("click", function () {
-      $("#correct,#wrong").off();
-  
-      $answerBtn1.removeClass("correct");
-      $answerBtn2.removeClass("wrong");
-      $answerBtn3.removeClass("wrong");
-      $answerBtn4.removeClass("wrong");
+  $question.html("1");
+  $answerBtn1.html("a1 card 1");
+  $answerBtn1.addClass("correct");
+
+  $answerBtn2.html("a2 card 1");
+  $answerBtn2.addClass("wrong");
+
+  $answerBtn3.html("a3 card 1");
+  $answerBtn3.addClass("wrong");
+
+  $answerBtn4.html("a4 card 1");
+  $answerBtn4.addClass("wrong");
+
+  $(".correct").on("click", function () {
+    $(".correct,.wrong").off();
+
+    $answerBtn1.removeClass("correct");
+    $answerBtn2.removeClass("wrong");
+    $answerBtn3.removeClass("wrong");
+    $answerBtn4.removeClass("wrong");
 
     card2();
   });
@@ -139,29 +153,29 @@ function card1() {
   $(".wrong").on("click", function () {
     timerStuff();
   });
-};   
+}
 
 function card2() {
-    $question.html("2");
-    $answerBtn1.html("a1 card 2");
-    $answerBtn1.addClass("wrong");
-  
-    $answerBtn2.html("a2 card 2");
-    $answerBtn2.addClass("correct");
-  
-    $answerBtn3.html("a3 card 2");
-    $answerBtn3.addClass("wrong");
-  
-    $answerBtn4.html("a4 card 2");
-    $answerBtn4.addClass("wrong");
-  
-    $(".correct").on("click", function () {
-      $(".correct,.wrong").off();
-  
-      $answerBtn1.removeClass("wrong");
-      $answerBtn2.removeClass("correct");
-      $answerBtn3.removeClass("wrong");
-      $answerBtn4.removeClass("wrong");
+  $question.html("2");
+  $answerBtn1.html("a1 card 2");
+  $answerBtn1.addClass("wrong");
+
+  $answerBtn2.html("a2 card 2");
+  $answerBtn2.addClass("correct");
+
+  $answerBtn3.html("a3 card 2");
+  $answerBtn3.addClass("wrong");
+
+  $answerBtn4.html("a4 card 2");
+  $answerBtn4.addClass("wrong");
+
+  $(".correct").on("click", function () {
+    $(".correct,.wrong").off();
+
+    $answerBtn1.removeClass("wrong");
+    $answerBtn2.removeClass("correct");
+    $answerBtn3.removeClass("wrong");
+    $answerBtn4.removeClass("wrong");
     card3();
   });
 
@@ -171,60 +185,61 @@ function card2() {
 }
 
 function card3() {
-    $question.html("3");
-    $answerBtn1.html("a1 card 3");
-    $answerBtn1.addClass("wrong");
-  
-    $answerBtn2.html("a2 card 3");
-    $answerBtn2.addClass("wrong");
-  
-    $answerBtn3.html("a3 card 3");
-    $answerBtn3.addClass("correct");
-  
-    $answerBtn4.html("a4 card 3");
-    $answerBtn4.addClass("wrong");
-  
-    $(".correct").on("click", function () {
-      $(".correct,.wrong").off();
-  
-      $answerBtn1.removeClass("wrong");
-      $answerBtn2.removeClass("wrong");
-      $answerBtn3.removeClass("correct");
-      $answerBtn4.removeClass("wrong");
+  $question.html("3");
+  $answerBtn1.html("a1 card 3");
+  $answerBtn1.addClass("wrong");
+
+  $answerBtn2.html("a2 card 3");
+  $answerBtn2.addClass("wrong");
+
+  $answerBtn3.html("a3 card 3");
+  $answerBtn3.addClass("correct");
+
+  $answerBtn4.html("a4 card 3");
+  $answerBtn4.addClass("wrong");
+
+  $(".correct").on("click", function () {
+    $(".correct,.wrong").off();
+
+    $answerBtn1.removeClass("wrong");
+    $answerBtn2.removeClass("wrong");
+    $answerBtn3.removeClass("correct");
+    $answerBtn4.removeClass("wrong");
     card4();
   });
 
-    $(".wrong").on("click", function () {
-        timerStuff();
-    });
-};
+  $(".wrong").on("click", function () {
+    timerStuff();
+  });
+}
 
 function card4() {
-    $question.html("4");
-    $answerBtn1.html("a1 card 4");
-    $answerBtn1.addClass("wrong");
-  
-    $answerBtn2.html("a2 card 4");
-    $answerBtn2.addClass("wrong");
-  
-    $answerBtn3.html("a3 card 4");
-    $answerBtn3.addClass("wrong");
-  
-    $answerBtn4.html("a4 card 4");
-    $answerBtn4.addClass("correct");
-  
-    $(".correct").on("click", function () {
-      $(".correct,.wrong").off();
-  
-      $answerBtn1.removeClass("wrong");
-      $answerBtn2.removeClass("wrong");
-      $answerBtn3.removeClass("wrong");
-      $answerBtn4.removeClass("correct");
+  $question.html("4");
+  $answerBtn1.html("a1 card 4");
+  $answerBtn1.addClass("wrong");
+
+  $answerBtn2.html("a2 card 4");
+  $answerBtn2.addClass("wrong");
+
+  $answerBtn3.html("a3 card 4");
+  $answerBtn3.addClass("wrong");
+
+  $answerBtn4.html("a4 card 4");
+  $answerBtn4.addClass("correct");
+
+  $(".correct").on("click", function () {
+    $(".correct,.wrong").off();
+
+
+    $answerBtn1.removeClass("wrong");
+    $answerBtn2.removeClass("wrong");
+    $answerBtn3.removeClass("wrong");
+    $answerBtn4.removeClass("correct");
 
     card5();
   });
 
   $(".wrong").on("click", function () {
-      timerStuff();
+    timerStuff();
   });
-};
+}
